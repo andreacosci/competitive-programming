9e6257605736c38511ae13d3f881ec1da9ad322b
andreacosci <42216798+andreacosci@users.noreply.github.com>
2019-10-01 14:00:22 +0200

    Add files via upload

---
+++ MissingNumber.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int testCase = 0, length = 0;
+	scanf("%d", &testCase);
+	
+	for(;testCase > 0; testCase--) {
+	    scanf("%d", &length);
+	    
+	    size_t sum = (length*(length + 1)) / 2;
+	    
+	    for(int i = 1; i < length; ++i) {
+			int tmp = 0;
+			scanf("%d", &tmp);
+			
+			sum -= tmp;
+		}
+		
+		std::cout << sum << '\n';
+	}
+	return 0;
+}
