4a6a6c27532e8180deb683764195d853a43ebb55
andreacosci <42216798+andreacosci@users.noreply.github.com>
2019-10-02 13:00:42 +0200

    Add files via upload

---
+++ NextLargerElement.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+inline void nextLarger(std::vector<long>& array) {
+	std::stack<long> stack;
+
+	for(auto it = array.rbegin(); it != array.rend(); ++it) {
+		while(!stack.empty() && *it >= stack.top()) {
+			stack.pop();	
+		}
+		
+		long val = *it;
+		
+		if (!stack.empty())
+			*it = stack.top();
+		else
+			*it = -1;
+			
+		stack.push(val);
+	} 
+	
+	for(const long& x : array)
+		std::cout << x << " ";
+	
+	std::cout << '\n';
+}
+
+int main() {
+	
+	int testCase = 0, length = 0;
+	std::vector<long> array;
+	
+	scanf("%d", &testCase);
+	while(testCase-- > 0) {
+		
+		scanf("%d", &length);
+		array.reserve(length);
+		
+		for(int j = 0; j < length; ++j) {
+			long tmp = 0;
+			scanf("%ld", &tmp);
+			array.push_back(tmp);
+		}
+		
+		nextLarger(array);
+		array.clear();
+	}
+	
+	return 0;
+}
