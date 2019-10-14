347128659e8dfc7aa197838cfdad514e44af15e4
andreacosci <42216798+andreacosci@users.noreply.github.com>
2019-10-14 17:53:49 +0200

    Add files via upload

---
+++ UpdateArray.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	
+	int t, n, u, q, v, l, r;
+	cin >> t;
+	while(t--) {
+		cin >> n >> u;
+		vector<int> diff(n + 1, 0);
+		
+		while(u--) {
+			cin >> l >> r >> v;
+			diff[l] += v;
+			diff[r + 1] -= v;
+		}
+		
+		for(int i = 1; i < n; i++) {
+			diff[i] += diff[i - 1];
+		}
+		
+		cin >> q;
+		int last = 0;
+		while(q--) {
+			cin >> v;
+			cout << diff[v] << '\n';
+		}
+	}
+}
