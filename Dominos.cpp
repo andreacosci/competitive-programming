95953786c230acf68cc34dd3c5c4d10d5729dca4
andreacosci <42216798+andreacosci@users.noreply.github.com>
2019-10-17 15:54:57 +0200

    Add files via upload

---
+++ Dominos.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main() {
+	
+	int h, w;
+	scanf("%d%d", &h, &w);
+	char grid[h + 1][w + 1];
+	
+	for(int i = 0; i < h; i++) {
+		grid[i][0] = '#';
+		scanf("%s", grid[i + 1] + 1);
+	}
+	for(int j = 1; j <= w; j++)
+		grid[0][j] = '#';
+	
+	int hor[h + 1][w + 1];
+	int ver[h + 1][w + 1];
+	
+	for(int i = 0; i <= h; i++) { 
+		memset(hor[i], 0, sizeof(hor[i])); 
+		memset(ver[i], 0, sizeof(hor[i])); 
+	}
+	
+	for(int i = 1; i <= h; i++) {
+		for(int j = 1; j <= w; j++) {
+			hor[i][j] = hor[i][j-1] + hor[i-1][j] + (grid[i][j] == '.' && grid[i][j-1] == '.') - hor[i-1][j-1];
+			ver[i][j] = ver[i-1][j] + ver[i][j-1] + (grid[i][j] == '.' && grid[i-1][j] == '.') - ver[i-1][j-1];
+		}
+	}
+	
+	int q;
+	scanf("%d", &q);
+	while(q--) {
+		int y1, x1, y2, x2;
+		scanf("%d%d%d%d", &y1, &x1, &y2, &x2);
+		int ans  = hor[y2][x2] - hor[y1 - 1][x2] - hor[y2][x1] + hor[y1 - 1][x1];
+		ans 	+= ver[y2][x2] - ver[y2][x1 - 1] - ver[y1][x2] + ver[y1][x1 - 1];
+		printf("%d\n", ans);
+	}
+	
+	
+}
