dc13224664bfa18069ebeda0922fabf716ad5543
andreacosci <42216798+andreacosci@users.noreply.github.com>
2020-02-04 10:39:59 +0100

    Update CircularRMQ.cpp

--- CircularRMQ.cpp
+++ CircularRMQ.cpp
@@ -1,86 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+ 
 using namespace std;
-typedef long long ll;
+typedef long long int ll;
 
-int n;
+vector<ll> sum;
+int n,m;
 
-class Problem {
-public:
-	
-	vector<ll> seg;
-	vector<ll> lazy;
-	const ll INF = 1e18;
-	
-	void setup() {
-		cin >> n;
-		seg = vector<ll>(16*n, 0);
-		lazy = vector<ll>(16*n, 0);
-		
-		for(int i = 0; i < n; i++) {
-			int tmp;
-			scanf("%d", &tmp);
-			add(i, i+1, tmp);
-		}
-	}
-	
-	void shift(int id) {
-		seg[2*id] 	 += lazy[id];
-		seg[2*id+1]  += lazy[id];
-		lazy[2*id]	 += lazy[id];
-		lazy[2*id+1] += lazy[id];
-		lazy[id]	 =  0;
-	}
-	
-	ll get(int ql, int qr, int l=0, int r=n, int id=1) {
-		if(qr <= ql) 
-			return min(get(ql, n), get(0, qr));
-		if(r <= ql || qr <= l) 
-			return INF;
-		if(ql <= l && r <= qr) 
-			return seg[id];
-		int mid = (l+r)/2;
-		shift(id);
-		return min( get(ql, qr, mid, r, 2*id+1), get(ql, qr, l, mid, 2*id));
-	}
-	
-	void add(int ql, int qr, ll val, int l=0, int r=n, int id=1) {
-		if (qr <= ql) {
-			add(ql, n, val) , add(0, qr, val);
-			return;
-		}
-		if(r <= ql || qr<=l ) return;
-		if(ql<=l && r<=qr) {
-			seg[id] += val;
-			lazy[id] += val;
-			return;
-		}
-		int mid = (l+r)/2;
-		shift(id);
-		add(ql, qr, val, l, mid, 2*id);
-		add(ql, qr, val, mid, r, 2*id+1);
-		seg[id] = min(seg[2*id],seg[2*id+1]);
-		return;
-	}
-};
-
-int main()
-{
-	Problem problem = Problem();
-	problem.setup();
-	
-	int m;
-	cin >> m;
-	
-	while(m--){ 
-		int l,r; char c;
-		scanf("%d %d%c",&l,&r,&c);
-		r++;
-		if(c == ' '){
-			ll val; 
-			cin >> val;
-			problem.add(l, r, val);
-		} else
-			cout << problem.get(l, r) << '\n';
+void print_tree() {
+	for(auto x : sum)
+		cout << x << " ";
+	cout << '\n';
+}
+ 
+int Update(int now, int L, int R, int v, int x, int y){
+	if(L <= x && y <= R){
+		sum[now]+=v;
+	} else {
+		int mid=(x+y)/2;
+		if(L<mid) Update(now*2,L,R,v,x,mid);
+		if(mid<R) Update(now*2+1,L,R,v,mid,y);
+		long long s = sum[now*2];
+		s = min(s,sum[now*2+1]);
+		sum[now] += s;
+		sum[now*2] -= s;
+		sum[now*2+1] -= s;
 	}
 	return 0;
+}
+ 
+long long Query(int now,int L,int R,int x,int y){
+	if(L<=x && y<=R)
+		return sum[now];
+	else{
+		int mid=(x+y)>>1;
+		long long s = LONG_LONG_MAX;
+		if(L<mid)s=min(s,Query(now*2,L,R,x,mid));
+		if(mid<R)s=min(s,Query(now*2+1,L,R,mid,y));
+		return sum[now]+s;
+	}
+}
+ 
+int main(){
+	int x,y,z;
+	char str[256];
+	
+	scanf("%d",&n);
+	int sz = (1 << static_cast<int>(ceil(log2(n))))*2;
+	sum = vector<ll>(sz, 0);
+	
+	for(int i = 0; i < n; i++){
+		scanf("%d",&x);
+		Update(1, i, i+1, x, 0, n);
+		print_tree();
+	}
+ 
+	scanf("%d",&m);
+	gets(str);
+	for(int i=0;i<m;i++){
+		if(sscanf(gets(str),"%d%d%d",&x,&y,&z)==3){
+			if(x<=y){
+				Update(1,x,y+1,z,0,n);
+			}else{
+				Update(1,x,n,z,0,n);
+				Update(1,0,y+1,z,0,n);
+			}
+		}else{
+			if(x<=y){
+				printf("%I64d\n", Query(1,x,y+1,0,n));
+			}else{
+				printf("%I64d\n",min(Query(1,x,n,0,n), Query(1,0,y+1,0,n)));
+			}
+		}
+	}
+		
 }
